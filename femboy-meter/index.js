1b2496b0813592fe552103eff4fd2fcdd770f6e7
jax <jaxon.baxon5@gmail.com>
2022-12-11 10:49:02 -0600

    ver0.3
    wrapper api update, bot can now be used in chats and not just home + more items

--- femboy-meter/index.js
+++ femboy-meter/index.js
@@ -12,7 +12,7 @@ let trade = {
     "sell": 15,
     "get": 20
 }
-const notes = 'ver0.2.1\nnotify command added + lucky number ooo'
+const notes = 'ver0.3\nwrapper api update, bot can now be used in chats and not just home + more items'
 const adapter = new JSONFile(join(__dirname, 'db/db.json'));
 const db = new Low(adapter);
 const shop = {
@@ -43,6 +43,13 @@ const shop = {
         'femgain': 80,
         'use-msg': ['You put on the maid dress, then the garments, and then everything... else.', 'The maid outfit is pretty tight on your femboy thighs.', 'Hey #, check it out!', 'Nya. Or something.', 'You are a femboy maid now.', 'The maid outfit suddenly makes you want to serve some plates at a diner.', 'You know, #, you don\'t have to stare.', 'You\'re pretty good at putting on this maid outfit, have you done it before?'],
         'unequip-msg': ['That was hard to take off.', 'Okay #, now you can look, it\'s off.', 'Not casual wear at all.', 'Why did I wear this again?', 'That felt nice for the first 30 minutes until I got hot.']
+    },
+    'febreze': {
+        'price': 25,
+        'description': 'Some febreze to keep your room smelling nice.',
+        'femgain': 0,
+        'use-msg': ['You sprayed some febreze, it smells nice.', 'The febreze smells wonderful.', 'You sprayed some febreze and then walked into it so that it sticks to you and you smell nice.', 'Great, your room isn\'t a biohazard now.', 'mmm....', 'The snail is getting closer.'],
+        'delete_on_use': true,
     }
 }
 
@@ -66,22 +73,22 @@ bot.on('post', async (message) => {
                 db.data[message.author].meter += Math.floor(Math.random() * 21) - 10;
                 if (db.data[message.author].meter > 100) db.data[message.author].meter = 100;
                 if (db.data[message.author].meter < 0) db.data[message.author].meter = 0;
-                bot.post(`You are ${db.data[message.author].meter}% a femboy!`)
+                bot.post(`You are ${db.data[message.author].meter}% a femboy!`, message.origin)
             } else {
                 db.data[message.author] = { meter: Math.floor(Math.random() * 101), fembucks: 0, items: [], equipped: [], lasttrade: 0, notify: false, luckynum: Math.floor(Math.random() * 51)}
-                bot.post(`You are ${db.data[message.author].meter}% a femboy!`)
+                bot.post(`You are ${db.data[message.author].meter}% a femboy!`, message.origin)
             }
         } else if (parsed.command == 'find') {
             if (parsed.args[0]) {
                 let user = parsed.args[0];
                 if (db.data.hasOwnProperty(user)) {
-                    bot.post(`${user} is ${db.data[user].meter}% a femboy!`)
+                    bot.post(`${user} is ${db.data[user].meter}% a femboy!`, message.origin)
                 } else {
-                    bot.post(`${user} is not in the database!`)
+                    bot.post(`${user} is not in the database!`, message.origin)
                 }
             }
         } else if (parsed.command == 'info') {
-            bot.post(`Hosting ${Object.keys(db.data).length} users with a custom wrapper. // made by jaxonbaxon#7560`)
+            bot.post(`Hosting ${Object.keys(db.data).length} users with a custom wrapper. // made by jaxonbaxon#7560`, message.origin)
         } else if (parsed.command == 'shop') {
             let assembly = []
             Object.keys(shop).forEach(item => {
@@ -90,11 +97,11 @@ bot.on('post', async (message) => {
             if (parsed.args.length > 0) {
                 assembly.forEach(item => {
                     if (item.startsWith(parsed.args[0])) {
-                        bot.post(item)
+                        bot.post(item, message.origin)
                     }
                 })
             } else {
-                bot.post(assembly.join('\n'))
+                bot.post(assembly.join('\n'), message.origin)
             }
         } else if (parsed.command == 'trade') {
             if (!db.data.hasOwnProperty(message.author)) return;
@@ -103,12 +110,12 @@ bot.on('post', async (message) => {
                     db.data[message.author].meter -= trade.sell
                     db.data[message.author].fembucks += trade.get
                     db.data[message.author].lasttrade = message.timestamp
-                    bot.post(`Traded ${trade.sell} points off your femboy meter for ${trade.get} fembucks!`)
+                    bot.post(`Traded ${trade.sell} points off your femboy meter for ${trade.get} fembucks!`, message.origin)
                 } else {
-                    bot.post('You don\'t have enough points on your femboy meter!')
+                    bot.post('You don\'t have enough points on your femboy meter!', message.origin)
                 }
             } else {
-                bot.post('Wait 1 minute until trading again!')
+                bot.post('Wait 1 minute until trading again!', message.origin)
             }
         } else if (parsed.command == 'buy' && db.data.hasOwnProperty(message.author)) {
             if (parsed.args.length > 0) {
@@ -116,23 +123,23 @@ bot.on('post', async (message) => {
                     if (db.data[message.author].fembucks >= shop[parsed.args[0]].price) {
                         db.data[message.author].fembucks -= shop[parsed.args[0]].price;
                         db.data[message.author].items.push(parsed.args[0]);
-                        bot.post(`You bought ${parsed.args[0]}!`)
+                        bot.post(`You bought ${parsed.args[0]}!`, message.origin)
                     } else {
-                        bot.post(`You don't have enough fembucks!`)
+                        bot.post(`You don't have enough fembucks!`, message.origin)
                     }
                 } else {
-                    bot.post(`That item doesn't exist!`)
+                    bot.post(`That item doesn't exist!`, message.origin)
                 }
             } else {
-                bot.post(`You didn't specify an item!`)
+                bot.post(`You didn't specify an item!`, message.origin)
             }
         } else if (parsed.command == 'fembucks' && db.data.hasOwnProperty(message.author)) {
-            bot.post(`You have ${db.data[message.author].fembucks} fembucks!`)
+            bot.post(`You have ${db.data[message.author].fembucks} fembucks!`, message.origin)
         } else if (parsed.command == 'items' && db.data.hasOwnProperty(message.author)) {
             if (db.data[message.author].items.length > 0) {
-                bot.post(db.data[message.author].items.join(', '))
+                bot.post(db.data[message.author].items.join(', '), message.origin)
             } else {
-                bot.post(`You don't have any items!`)
+                bot.post(`You don't have any items!`, message.origin)
             }
         } else if (parsed.command == 'equip' && db.data.hasOwnProperty(message.author)) {
             if (parsed.args[0]) {
@@ -147,12 +154,12 @@ bot.on('post', async (message) => {
                         let use = shop[parsed.args[0]]['use-msg'][Math.floor(Math.random() * shop[parsed.args[0]]['use-msg'].length)]
                         db.data[message.author].meter += shop[parsed.args[0]].femgain
                         let ulist = bot.get('ulist')
-                        bot.post(use.replace(/#/g, ulist[Math.floor(Math.random() * ulist.length)]))
+                        bot.post(use.replace(/#/g, ulist[Math.floor(Math.random() * ulist.length)]), message.origin)
                     } else {
-                        bot.post(`You can't equip that!`)
+                        bot.post(`You can't equip that!`, message.origin)
                     }
                 } else {
-                    bot.post(`You don't have that item!`)
+                    bot.post(`You don't have that item!`, message.origin)
                 }
             }
         } else if (parsed.command == 'unequip' && db.data.hasOwnProperty(message.author)) {
@@ -162,33 +169,33 @@ bot.on('post', async (message) => {
                     db.data[message.author].items.push(parsed.args[0])
                     db.data[message.author].femgain -= (Math.abs(shop[parsed.args[0]]['femgain']) / 2)
                     if (shop[parsed.args[0]].hasOwnProperty('unequip-msg')) {
-                        bot.post(shop[parsed.args[0]]['unequip-msg'][Math.floor(Math.random() * shop[parsed.args[0]]['unequip-msg'].length)])
+                        bot.post(shop[parsed.args[0]]['unequip-msg'][Math.floor(Math.random() * shop[parsed.args[0]]['unequip-msg'].length)], message.origin)
                     } else {
-                        bot.post(`You unequipped ${parsed.args[0]}.`)
+                        bot.post(`You unequipped ${parsed.args[0]}.`, message.origin)
                     }
                 } else {
-                    bot.post(`You don't have that item equipped!`)
+                    bot.post(`You don't have that item equipped!`, message.origin)
                 }
             }
         } else if (parsed.command == 'equipped' && db.data.hasOwnProperty(message.author)) {
             if (parsed.args[0]) {
                 if (db.data[message.author].equipped.includes(parsed.args[0])) {
-                    bot.post(`You have ${parsed.args[0]} equipped.`)
+                    bot.post(`You have ${parsed.args[0]} equipped.`, message.origin)
                 } else {
-                    bot.post(`You do not have ${parsed.args[0]} equipped.`)
+                    bot.post(`You do not have ${parsed.args[0]} equipped.`, message.origin)
                 }
             } else {
-                bot.post(db.data[message.author].equipped.join(', '))
+                bot.post(db.data[message.author].equipped.join(', '), message.origin)
             }
         } else if (parsed.command == 'stocks') {
-            bot.post(`sell price: ${trade.sell}\nreturn: ${trade.get}`)
+            bot.post(`sell price: ${trade.sell}\nreturn: ${trade.get}`, message.origin)
         } else if (parsed.command == 'update') {
-            bot.post(notes)
+            bot.post(notes, message.origin)
         } else if (parsed.command == 'help') {
-            bot.post('https://femboy-meter-docs.jaxonbaxon.repl.co')
+            bot.post('https://femboy-meter-docs.jaxonbaxon.repl.co', message.origin)
         } else if (parsed.command == 'notify' && db.data.hasOwnProperty(message.author)) {
             db.data[message.author].notify = !db.data[message.author].notify
-            bot.post(`Set notifications to ${db.data[message.author].notify}`)
+            bot.post(`Set notifications to ${db.data[message.author].notify}`, message.origin)
         }
         await db.write()
     }
